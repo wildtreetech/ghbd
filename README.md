4ecd5da92bb1360e2c6d2f959edef88fcabcfaea
Tim Head <betatim@gmail.com>
2016-06-26 16:25:53 +0200

    Churning the churn

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Material for [Geneva's Humanitarian Big Data](www.wildtreetech.com/ghbd).
 
 By the end you will know:
 
-* the basics of []`scikit-learn`](http://scikit-learn.org/stable/)
+* the basics of [`scikit-learn`](http://scikit-learn.org/stable/)
 * how to use Decision Trees and Random Forests
 * how to use cross-validation to measure performance
 * that there are many metrics by which to measure performance
@@ -29,8 +29,8 @@ to get the python3.5 version.
 
 <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Geneva's Humanitarian Big Data</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="https://github.com/wildtreetech/ghbd" property="cc:attributionName" rel="cc:attributionURL">Tim Head</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.
 
-[visualtrees]: (http://www.r2d3.us/visual-intro-to-machine-learning-part-1/)
-[privatestats]: (http://blog.chromium.org/2014/10/learning-statistics-with-privacy-aided.html)
+[visualtrees]: http://www.r2d3.us/visual-intro-to-machine-learning-part-1/
+[privatestats]: http://blog.chromium.org/2014/10/learning-statistics-with-privacy-aided.html
 
 
 # Material
--- utils.py
+++ utils.py
@@ -1,10 +1,17 @@
 import subprocess
 from tempfile import mkstemp
+from itertools import cycle
 
 import matplotlib.pyplot as plt
 import numpy as np
+
+from scipy import interp
+
 from sklearn.tree import export_graphviz
+from sklearn.metrics import roc_curve, auc
+
 from IPython.core.display import HTML
+
 
 
 def plot_surface(clf, X, y,
@@ -42,3 +49,38 @@ def draw_tree(clf, feature_names, **kwargs):
     command = ["dot", "-Tsvg", name, "-o", svg_name]
     subprocess.check_call(command)
     return HTML(open(svg_name).read())
+
+
+# Taken from http://scikit-learn.org/stable/auto_examples/model_selection/plot_roc_crossval.html
+def draw_roc_curve(classifier, cv, X, y):
+    mean_tpr = 0.0
+    mean_fpr = np.linspace(0, 1, 100)
+
+    colors = cycle(['cyan', 'indigo', 'seagreen', 'yellow', 'blue', 'darkorange'])
+    lw = 2
+
+    for i, (train, test) in enumerate(cv):
+        probas_ = classifier.fit(X[train], y[train]).predict_proba(X[test])
+        # Compute ROC curve and area the curve
+        fpr, tpr, thresholds = roc_curve(y[test], probas_[:, 1])
+        mean_tpr += interp(mean_fpr, fpr, tpr)
+        mean_tpr[0] = 0.0
+        roc_auc = auc(fpr, tpr)
+        plt.plot(fpr, tpr, lw=1, label='ROC fold %d (area = %0.2f)' % (i, roc_auc))
+
+    plt.plot([0, 1], [0, 1], linestyle='--', lw=lw, color='k',
+             label='Luck')
+
+    mean_tpr /= len(cv)
+    mean_tpr[-1] = 1.0
+    mean_auc = auc(mean_fpr, mean_tpr)
+    plt.plot(mean_fpr, mean_tpr, color='g', linestyle='--',
+             label='Mean ROC (area = %0.2f)' % mean_auc, lw=lw)
+
+    plt.xlim([-0.05, 1.05])
+    plt.ylim([-0.05, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic')
+    plt.legend(loc="lower right")
+    plt.show()
